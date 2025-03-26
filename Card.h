a5de7346e521cc3c6b1cd5e82d6d1925e70bcdae
gurnoorjande <gjande@uwo.ca>
2025-03-25 20:15:44 -0400

    initial commit

---
+++ Card.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Card {
+public:
+    enum Suit 
+    {
+        HEARTS,
+        DIAMONDS,
+        SPADES,
+        CLUBS,
+        SUIT
+    };
+    enum Rank 
+    {
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING,
+        ACE,
+        RANK
+    };
+    Card();
+    Card(Suit suit, Rank rank);
+    Suit getSuit() const;
+    Rank getRank() const;
+    string show() const;
+    int get_value() const;
+
+private:
+    Suit suit_;
+    Rank rank_;
+};
